a7188374c4991c80c4db66298b6512e182b4299e
letele <mtletele@gmail.com>
2023-01-22 02:56:20 -0800

    Edited hearts svg

--- src/Hearts.js
+++ src/Hearts.js
@@ -10,90 +10,36 @@ const SvgHearts = ({ title, titleId, ...props }) => (
     {...props}
   >
     {title ? <title id={titleId}>{title}</title> : null}
-    <rect
-      width={10.698}
-      height={14.749}
-      x={1.936}
-      y={0.372}
-      rx={0.537}
-      ry={0.528}
-      fill="#fff"
-      stroke="#000"
-      style={{
-        strokeWidth: 0.172805,
-        strokeDasharray: "none",
-        fill: "none",
-      }}
-    />
-    <path
-      d="M-260 100c300 0 220 360 520 360M-175 0v-285a175 175 0 0 1 350 0v570a175 175 0 0 1-350 0z"
-      stroke="#000"
-      strokeWidth={80}
-      strokeLinecap="square"
-      strokeMiterlimit={1.5}
-      fill="none"
-      transform="matrix(.00143 0 0 .0014 2.88 1.583)"
-    />
-    <path d="M2.88 2.606c.1.245.355.393.355.673a.15.147 0 0 1-.3 0 .01.01 0 0 0-.02 0c0 .196.05.21.095.309h-.26c.046-.098.096-.113.096-.31a.01.01 0 0 0-.02 0 .15.147 0 0 1-.3 0c0-.28.255-.427.355-.672z" />
-    <rect
-      width={10.698}
-      height={14.749}
-      x={3.366}
-      y={0.88}
-      rx={0.537}
-      ry={0.528}
-      fill="#fff"
-      stroke="#000"
-      style={{
-        strokeWidth: 0.172805,
-        strokeDasharray: "none",
-      }}
-    />
-    <path
-      d="M0-300c0-100 100-200 200-200s200 100 200 250C400 0 0 400 0 500 0 400-400 0-400-250c0-150 100-250 200-250S0-400 0-300z"
-      fill="red"
-      transform="matrix(.00261 0 0 .00257 8.714 8.254)"
-      style={{
-        strokeWidth: 3.89276,
-        strokeDasharray: "none",
-      }}
-    />
-    <path
-      d="M-270 460h160m-90-10L0-460l200 910m-90 10h160m-390-330h240"
-      stroke="red"
-      strokeWidth={80}
-      strokeLinecap="square"
-      strokeMiterlimit={1.5}
-      fill="none"
-      transform="matrix(.00143 0 0 .0014 4.31 2.09)"
-      style={{
-        strokeWidth: 3.89276,
-        strokeDasharray: "none",
-      }}
-    />
-    <path
-      d="M0-300c0-100 100-200 200-200s200 100 200 250C400 0 0 400 0 500 0 400-400 0-400-250c0-150 100-250 200-250S0-400 0-300z"
-      fill="red"
-      transform="matrix(.001 0 0 .00098 4.31 3.604)"
-      style={{
-        strokeWidth: 3.89276,
-        strokeDasharray: "none",
-      }}
-    />
-    <g
-      style={{
-        strokeWidth: 3.89276,
-        strokeDasharray: "none",
-      }}
-    >
+    <g transform="translate(-.714 -.254)">
+      <rect
+        width={10.698}
+        height={14.749}
+        x={3.366}
+        y={0.88}
+        rx={0.537}
+        ry={0.528}
+        fill="#fff"
+        stroke="#000"
+        style={{
+          strokeWidth: 0.5,
+          strokeDasharray: "none",
+        }}
+      />
+      <path d="M12.863 15.295c.21-.517.746-.827.746-1.417a.315.31 0 0 0-.63 0 .021.02 0 0 1-.043 0c0-.413.106-.444.2-.651h-.546c.094.207.2.238.2.651a.021.02 0 0 1-.043 0 .315.31 0 0 0-.63 0c0 .59.536.9.746 1.417z" />
       <path
-        d="M-270 460h160m-90-10L0-460l200 910m-90 10h160m-390-330h240"
-        stroke="red"
+        d="M-260 100c300 0 220 360 520 360M-175 0v-285a175 175 0 0 1 350 0v570a175 175 0 0 1-350 0z"
+        stroke="#000"
         strokeWidth={80}
         strokeLinecap="square"
         strokeMiterlimit={1.5}
         fill="none"
-        transform="matrix(-.00143 0 0 -.0014 13.119 14.418)"
+        transform="matrix(.00557 0 0 .00548 6.809 4.247)"
+      />
+      <path d="M6.05 13.74c.556-1.367 1.973-2.188 1.973-3.746a.834.82 0 0 0-1.668 0 .056.055 0 0 1-.11 0c0-1.094.277-1.176.527-1.722H5.327c.25.546.528.628.528 1.722a.056.055 0 0 1-.111 0 .834.82 0 0 0-1.668 0c0 1.558 1.418 2.379 1.974 3.746z" />
+      <path
+        d="M0-300c0-100 100-200 200-200s200 100 200 250C400 0 0 400 0 500 0 400-400 0-400-250c0-150 100-250 200-250S0-400 0-300z"
+        fill="red"
+        transform="matrix(.0052 0 0 .00512 11.086 5.702)"
         style={{
           strokeWidth: 3.89276,
           strokeDasharray: "none",
@@ -102,7 +48,20 @@ const SvgHearts = ({ title, titleId, ...props }) => (
       <path
         d="M0-300c0-100 100-200 200-200s200 100 200 250C400 0 0 400 0 500 0 400-400 0-400-250c0-150 100-250 200-250S0-400 0-300z"
         fill="red"
-        transform="matrix(-.001 0 0 -.00098 13.119 12.904)"
+        transform="matrix(.0017 0 0 .00167 4.5 2.243)"
+        style={{
+          strokeWidth: 3.89276,
+          strokeDasharray: "none",
+        }}
+      />
+      <path
+        d="M-270 460h160m-90-10L0-460l200 910m-90 10h160m-390-330h240"
+        stroke="red"
+        strokeWidth={80}
+        strokeLinecap="square"
+        strokeMiterlimit={1.5}
+        fill="none"
+        transform="matrix(-.00587 0 0 -.00577 10.846 12.103)"
         style={{
           strokeWidth: 3.89276,
           strokeDasharray: "none",
